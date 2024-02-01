700c577d9d2c48c9bd2924c74ff71b822d5e532a
col-yuze <sr.yuze98@gmail.com>
2024-02-01 11:12:10 +0200

    lastUpdated

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -3,15 +3,17 @@ import {
   closeDatabaseConnection,
   runQuery,
 } from "../../lib/db";
+console.log("connected1");
 
 export default async function handler(req, res) {
   let connection;
   try {
     connection = await connectToDatabase();
-
+    console.log("connected2");
     // Your logic here, you can use the `runQuery` function
     const result = await runQuery("SELECT * FROM PATIENT");
     res.status(200).json(result);
+    console.log("thanks");
   } catch (error) {
     console.error("Error in API route:", error);
     res.status(500).json({ error: "Internal Server Error" });
