7e34bb89bf91cd0dce831c70c50be67b61d0a342
col-yuze <sr.yuze98@gmail.com>
2024-02-04 14:36:23 +0200

    api endpoints skeleton

--- src/app/nozomreports/nesbetashghal/page.js
+++ src/app/nozomreports/nesbetashghal/page.js
@@ -8,108 +8,124 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import fetchData from "./nesbtAsh8al"
-import { DotLoader } from 'react-spinner-overlay'
+import fetchData from "./nesbtAsh8al";
 
 export default function Nesbestashghal() {
-const [rows,setRows] = React.useState([])
-const headers = [
-  "",
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "h7",
-  "h8",
-  "h9",
-  "h10",
-];
+  const [rows, setRows] = React.useState([]);
+  const headers = [
+    "",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "h7",
+    "h8",
+    "h9",
+    "h10",
+  ];
 
-const handleSaveAsPDF = async () => {
-  // Dynamically import html2pdf only on the client-side
-  const html2pdf = (await import("html2pdf.js")).default;
-  
-  const content = document.getElementById("pdf-container");
-  
-  if (!content) {
-    console.error("Could not find PDF container");
-    return;
-  }
-  
-  const pdfOptions = {
-    margin: 10,
-    filename: "table.pdf",
-    image: { type: "jpeg", quality: 0.98 },
-    html2canvas: { scale: 2 },
-    jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-  };
-  html2pdf().from(content).set(pdfOptions).save();
-  };
+  const handleSaveAsPDF = async () => {
+    // Dynamically import html2pdf only on the client-side
+    const html2pdf = (await import("html2pdf.js")).default;
 
+    const content = document.getElementById("pdf-container");
 
-React.useEffect(() => {
-  let isMounted = true; // Variable to check if the component is still mounted
-
-  const fetchDataAndSetRows = async () => {
-    try {
-      const responseData = await fetchData();
-      if (isMounted) {
-        setRows(responseData.data.map((el, index) => ({ name: `row${index + 1}`, data: el })));
-        console.log("Data fetched and rows updated");
-      }
-    } catch (error) {
-      console.log("Error fetching data:", error);
+    if (!content) {
+      console.error("Could not find PDF container");
+      return;
     }
+
+    const pdfOptions = {
+      margin: 10,
+      filename: "table.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    };
+    html2pdf().from(content).set(pdfOptions).save();
   };
 
-  if (typeof window !== 'undefined') {
-    fetchDataAndSetRows();
-  }
+  React.useEffect(() => {
+    let isMounted = true; // Variable to check if the component is still mounted
 
-  return () => {
-    // Cleanup function to set isMounted to false when the component is unmounted
-    isMounted = false;
-  };
-}, []);
-  
+    const fetchDataAndSetRows = async () => {
+      try {
+        const responseData = await fetchData();
+        if (isMounted) {
+          setRows(
+            responseData.data.map((el, index) => ({
+              name: `row${index + 1}`,
+              data: el,
+            }))
+          );
+          console.log("Data fetched and rows updated");
+        }
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    if (typeof window !== "undefined") {
+      fetchDataAndSetRows();
+    }
+
+    return () => {
+      // Cleanup function to set isMounted to false when the component is unmounted
+      isMounted = false;
+    };
+  }, []);
+
   return (
     <div style={{ padding: 50 }}>
       <div style={{ paddingInline: "15%" }}>
         <div id="pdf-container">
           <h1 style={{ marginBottom: 20, color: "#F0ECE5" }}>نسبة الاشغال</h1>
-          {rows.length <= 0 ? <div style={{display:'flex',justifyContent:"center",alignItems:"center",minHeight:500}}> <DotLoader size={10} between={5} /> </div> : <TableContainer
-            component={Paper}
-            style={{ backgroundColor: "#F0ECE5" }}
-          >
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  {headers.map((header, index) => (
-                    <TableCell align="center" key={index}>
-                      {header}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow
-                    key={row.name}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell scope="row">{row.name}</TableCell>
-                    {row.data.map((el, index) => (
-                      <TableCell key={index} align="center">
-                        {el}
+          {rows.length <= 0 ? (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                minHeight: 500,
+              }}
+            >
+              {" "}
+            </div>
+          ) : (
+            <TableContainer
+              component={Paper}
+              style={{ backgroundColor: "#F0ECE5" }}
+            >
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    {headers.map((header, index) => (
+                      <TableCell align="center" key={index}>
+                        {header}
                       </TableCell>
                     ))}
                   </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>}
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell scope="row">{row.name}</TableCell>
+                      {row.data.map((el, index) => (
+                        <TableCell key={index} align="center">
+                          {el}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          )}
         </div>
         <div style={{ alignSelf: "center" }}>
           <Button
