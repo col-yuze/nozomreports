e4819a6c90ea6054ab496273c48efe5b18092b1c
yuze <sr.yuze98@gmail.com>
2024-02-21 09:31:21 +0200

    added title

--- src/app/nozomreports/adweya/page.js
+++ src/app/nozomreports/adweya/page.js
@@ -114,7 +114,7 @@ export default function Adweya() {
             </div>
           ) : (
             <DynamicPDFViewer showToolbar={true} width="100%" height="720px">
-              <MyDocument data={rows} />
+              <MyDocument data={rows} title="الادوية" />
             </DynamicPDFViewer>
           )}
         </div>
