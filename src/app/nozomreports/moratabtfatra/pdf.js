a5e822df93fad300a6e0c1b33658d1ec2d8efbe8
genflob <generalflob@gmail.com>
2024-03-16 12:55:36 +0200

    moratabatfatraPDF_update

--- src/app/nozomreports/moratabtfatra/pdf.js
+++ src/app/nozomreports/moratabtfatra/pdf.js
@@ -13,18 +13,6 @@ import NotoNaskh from "../../../styles/ReadexPro-VariableFont_HEXP,wght.otf";
 Font.register({ family: "NotoNaskh", src: NotoNaskh });
 
 // Define styles
-const departments = [
-  "الكل",
-  "مستشفى الجراحة",
-  "مستشفى الباطنة",
-  "مستشفى الجهاز التنفسي",
-  "مستشفى الاسنان التخصصي",
-  "الاستقبال و الطوارئ و الحوادث",
-  "مستشفى الكلى",
-  "مستشفى القلب التخصصي",
-  "مستشفى العيون التخصصي",
-  "السموم",
-];
 const styles = StyleSheet.create({
   page: {
     flexDirection: "row-reverse",
@@ -102,35 +90,22 @@ const styles = StyleSheet.create({
 
 const rowsPerPageTitled = 29; // Adjusted for the first page which includes the title
 const rowsPerPage = 33; // For subsequent pages
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, "0");
-var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-var yyyy = today.getFullYear();
 
-today = dd + "-" + mm + "-" + yyyy;
 const MyDocument = ({ data, title }) => {
-  // Your helper functions and logic remain unchanged
-  const [isToday, setIsToday] = React.useState(false);
-
   // Dynamically split data into pages considering different row limits
   const pagesData = [];
   let index = 0;
   let isFirstPage = true;
-  //console.log(data[0][1].slice(0, 1));
   const titlePages = [];
   for (let j = 0; j < data.length; j++) {
     isFirstPage = true;
     if (data[j][1].length > 0) {
       titlePages.push(pagesData.length);
-      var arrayTop = [
-        [departments[data[j][0]], "العدد", data[j][1].length - 1],
-      ];
+      var arrayTop = [[]];
       while (index < data[j][1].length) {
         const limit = isFirstPage ? rowsPerPageTitled : rowsPerPage;
         var arr = [];
-        //if (index == 0)
         arr = arrayTop.concat(data[j][1].slice(index, index + limit));
-        //else arr = data[j][1].slice(index, index + limit);
         pagesData.push(arr);
 
         index += limit;
@@ -139,8 +114,7 @@ const MyDocument = ({ data, title }) => {
       index = 0;
     }
   }
-  console.log(today);
-  console.log(typeof today);
+
   return (
     <Document>
       {pagesData.map((pageData, pageIndex) => (
@@ -151,11 +125,7 @@ const MyDocument = ({ data, title }) => {
                 <Text style={styles.title}>{title}</Text>
               </View>
             )}
-            {/* {titlePages.includes(pageIndex) && (
-              <View style={styles.titleContainer}>
-                <Text style={styles.title}>KOKI</Text>
-              </View>
-            )} */}
+            {}
             <View style={styles.table}>
               {pageData.map((rowData, index) => (
                 <View
@@ -164,11 +134,9 @@ const MyDocument = ({ data, title }) => {
                     {
                       backgroundColor:
                         index === 1 && titlePages.includes(pageIndex)
-                          ? "#d4d8dd"
-                          : index === 0
                           ? "#e1e1e1"
-                          : rowData[3] === today
-                          ? "#e1e1e1"
+                          : index === 2
+                          ? "#ffe0e0"
                           : "white",
                     },
                   ]}
@@ -180,30 +148,14 @@ const MyDocument = ({ data, title }) => {
                         styles.cell,
                         {
                           fontSize: "7px",
-                          flex:
-                            cellIndex === 2
-                              ? "2.5"
-                              : cellIndex === 0
-                              ? "0.15"
-                              : cellIndex === 4
-                              ? "1.2"
-                              : cellIndex === 3
-                              ? "0.6"
-                              : cellIndex === 1
-                              ? "0.6"
-                              : "0.9",
+                          alignSelf: "center",
                         },
-                        index === 0 //&& titlePages.includes(pageIndex)
+                        index === 0
                           ? {
                               paddingTop: "15px",
                               paddingBottom: "15px",
                               fontSize: "12px",
-                              flex:
-                                cellIndex === 0
-                                  ? "3.425"
-                                  : cellIndex === 1
-                                  ? "1.875"
-                                  : "0.875",
+                              alignSelf: "center",
                             }
                           : null,
                       ]}
