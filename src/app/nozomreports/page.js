f2bcd0e1e0d96826fcfd9e00cedc507bb44025ec
col-yuze <sr.yuze98@gmail.com>
2024-04-01 08:49:50 +0200

    changed updates

--- src/app/nozomreports/page.js
+++ src/app/nozomreports/page.js
@@ -5,32 +5,32 @@ import NavBar from "./../../components/NavBar.js";
 
 const items = [
   { title: "نسبة الاشغال", route: "nesbetashghal" }, //done input skeleton
-  { title: "احصائية الادوية المنصرفة بالصيدليات خلال فترة", route: "adweya" }, //done input skeleton
-  {
-    title: "احصائية الادوية المنصرفة بالقسم الداخلي عن يوم",
-    route: "adweyadakhly",
-  }, //done input skeleton
-  { title: "العيادات", route: "adweyaeyadat" }, //done input skeleton
-  { title: "الرمد", route: "adweyaramad" }, //done input skeleton
-  { title: "تقرير المحجوزين بالمجمع خلال فترة", route: "mahgoozfatra" }, //done input skeleton
   { title: " احصائية العيادات اليومية", route: "ehsaeyaeyadat" }, //done input skeleton
-  { title: "احصائيةالمرتبات لاكثر من 10 ادوية", route: "ehsaeyamoratabatten" }, //done input skeleton
   { title: "احصائية القدوم اليومية", route: "ehsaeyaqodoom" }, //done input skeleton
-  { title: "تقرير بالمحجوزين حاليا بالمجمع", route: "mahgozeen" }, //done input skeleton
-  { title: "تقرير مرضي لم يتم تسجيل خروجهم", route: "mardanotout" }, //done input skeleton
-  { title: "مرتبات علاجية منصرفة لمريض خلال فترة", route: "moratabtfatra" }, //done input skeleton
+  { title: "احصائية العياادات تخصصات", route: "takhasosateyadat" }, //done input skeleton
+  { title: "العيادات", route: "adweyaeyadat" }, //done input skeleton
+  { title: "الرمد (عيادات)", route: "adweyaramad" }, //done input skeleton
   { title: "احصائية المرتبات العلاجية", route: "moratbat" }, //done input skeleton
+  { title: "تقرير وافدين (دخلي)", route: "wafedeendakhly" }, //done input skeleton
+  { title: "تقرير الوافدين (عيادات)", route: "wafedeeneyadat" }, //done input skeleton
+  { title: "احصائيةالمرتبات لاكثر من 10 ادوية", route: "ehsaeyamoratabatten" }, //done input skeleton
   {
     title: "تقرير صرف مرتبات اكثر من 10 ادوية",
     route: "sarfmoratabatten",
   },
-  { title: "تخصصات", route: "takhasosat" }, //done input skeleton
-  { title: "تقرير وافدين (دخلي)", route: "wafedeendakhly" }, //done input skeleton
-  { title: "تقرير الوافدين (عيادات)", route: "wafedeeneyadat" }, //done input skeleton
-  { title: "احصائية بالاعداد المتاحة في العيادات", route: "aadadmotaha" }, // no need for input skeleton
-  { title: "تقرير الادوية المنصرفة لصالح قسم دخلي", route: "taqreeradweya" }, //done input skeleton
-  { title: "احصائية العياادات تخصصات", route: "takhasosateyadat" }, //done input skeleton
+  { title: "تقرير مرضي لم يتم تسجيل خروجهم", route: "mardanotout" }, //done input skeleton
+  { title: "تقرير المحجوزين بالمجمع خلال فترة", route: "mahgoozfatra" }, //done input skeleton
+  { title: "تقرير بالمحجوزين حاليا بالمجمع", route: "mahgozeen" }, //done input skeleton
+  { title: "احصائية الادوية المنصرفة بالصيدليات خلال فترة", route: "adweya" }, //done input skeleton
   { title: "احصائية الادوية المنصرفة مرتبات علاجية", route: "adweyamoratabat" }, // no need for input skeleton
+  { title: "احصائية الادوية المنصرفة مرتبات علاجية_تخصصات", route: "takhasosat" }, //done input skeleton
+  { title: "تقرير الادوية المنصرفة لصالح قسم دخلي", route: "taqreeradweya" }, //done input skeleton
+  {
+    title: "احصائية الادوية المنصرفة بالقسم الداخلي عن يوم",
+    route: "adweyadakhly",
+  }, //done input skeleton
+  { title: "مرتبات علاجية منصرفة لمريض خلال فترة", route: "moratabtfatra" }, //done input skeleton
+  { title: "احصائية بالاعداد المتاحة في العيادات", route: "aadadmotaha" }, // no need for input skeleton
 ];
 
 export default function nozomreports() {
